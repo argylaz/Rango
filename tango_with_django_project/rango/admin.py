190c83e2ee9558cd0e8a8e0d5a5a4f58e0ce8a0d
argylaz <argyrhslazaridhs@gmail.com>
2024-02-09 18:22:11 +0200

    6 - Completed - Passing tests

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-# Registering the Category and Page models with the admin interface
-admin.site.register(Category)
+# New class to automatically fill n slug field when a new category is created
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 # New class to customise the admin interface
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-# Register new class to update interface
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
+
+# Register Category and Page models with the admin interface
 admin.site.register(Page, PageAdmin)
 
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Category model
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    # Overwriting save to automatically create a slug
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs) # Call the real save() method
 
     # Just fixing the plural of category
     class Meta:

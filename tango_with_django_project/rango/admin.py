ede4fda224235916bd3f9f479e566bdd3dcce48b
argylaz <argyrhslazaridhs@gmail.com>
2024-02-09 15:12:15 +0200

    5 - Completed - Passing all tests

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+# Registering the Category and Page models with the admin interface
+admin.site.register(Category)
+
+# New class to customise the admin interface
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+# Register new class to update interface
+admin.site.register(Page, PageAdmin)
+
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    # Just fixing the plural of category
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+# Page model
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
